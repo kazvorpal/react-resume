6911d24ca9dcb3b27a467148ae4cf557aab51a3a
Kaz <kazmichaelkarl@gmail.com>
2024-09-18 14:51:23 -0400

    Committing some old changes, and adding some header info to make it a bit less sparse

---
+++ js/general.js
@@ -0,0 +1,7 @@
+contactinfo = new Map([
+   ["Phone", "tel:917-826-9944"],
+   ["E-Mail", "mailto:kazmichaelkarl@gmail.com"],
+   ["LinkedIn", "https://www.linkedin.com/in/kazmichaelkarl/"],
+   ["GitHub (this repo)", "https://github.com/kazvorpal/resume-react"], 
+   ["ChatGPT Agent taught to advocate for Kaz via RAG", "https://chatgpt.com/g/g-twnLEOznQ-the-ai-agent-of-kaz"]
+])
