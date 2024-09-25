66332757f900f655300d681ad4273c0f12c4b0ca
Kaz <kazmichaelkarl@gmail.com>
2024-09-25 19:41:48 -0400

    RAG (Retrieval-Augmented Generation) is too technical

--- js/general.js
+++ js/general.js
@@ -3,6 +3,6 @@ contactinfo = new Map([
    ["E-Mail", "mailto:kazmichaelkarl@gmail.com"],
    ["LinkedIn", "https://www.linkedin.com/in/kazmichaelkarl/"],
    ["GitHub (this repo)", "https://github.com/kazvorpal/resume-react"], 
-   ["ChatGPT Agent taught to advocate for Kaz via RAG", "https://chatgpt.com/g/g-twnLEOznQ-the-ai-agent-of-kaz"],
+   ["ChatGPT Agent taught to advocate for Kaz", "https://chatgpt.com/g/g-twnLEOznQ-the-ai-agent-of-kaz"],
    ["Resume in PDF format", "Kaz-resume-ats.pdf"]
 ])
